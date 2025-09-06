692fb0444229e9589be5b709e1fb78a2b8c773ae
Jack Le <jackl32482005@gmail.com>
2025-09-06 18:33:52 -0400

    Initital edits

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,8 +6,8 @@ const header = {
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Jack Le',
+  role: 'Software Developer',
   picture: 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
 
   description:
